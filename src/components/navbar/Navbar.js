b20e75052ffdbaa53d009bc06c1869bf08671ae0
Klug007 <tilla.yosinbek@gmail.com>
2024-02-11 14:33:53 +0500

    Added Sidebar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
+import Logo from "../../assets/byd-logo.png";
 import { CiCircleQuestion } from "react-icons/ci";
 import { TbWorld, TbUserCircle } from "react-icons/tb";
 import { nav_data } from "../../static/static_data";
+import { IoIosMenu } from "react-icons/io";
 import NavChild from "./navChild/NavChild";
+import SideBar from "./sideBar/SideBar";
 
 function Navbar() {
     const [open, setOpen] = useState(false);
+    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth < 990);
+    const [openMenu, setOpenMenu] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWideScreen(window.innerWidth < 990);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        // Call handleResize initially to set the initial state
+        handleResize();
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <div className="navbar">
@@ -18,7 +37,7 @@ function Navbar() {
                     }`}
                 >
                     <a href="/">
-                        <Logo />
+                        <img src={Logo} alt="" />
                     </a>
                 </div>
             </div>
@@ -28,11 +47,14 @@ function Navbar() {
                 }`}
             >
                 <ul className="navbar__ul">
-                    {
-                        nav_data.map(nav => (
-                            <NavChild setOpen={setOpen} nav={nav} open={open}/>
-                        ))
-                    }
+                    {nav_data.map((nav) => (
+                        <NavChild
+                            setOpen={setOpen}
+                            nav={nav}
+                            open={open}
+                            key={nav.id}
+                        />
+                    ))}
                 </ul>
                 <div
                     className="background_blur"
@@ -46,27 +68,42 @@ function Navbar() {
             </div>
             <div className="navbar__item">
                 <div
-                    className={`navbar__logos ${
+                    className={`navbar__icons ${
                         open ? `color-black` : `color-white`
                     }`}
                 >
-                    <ul>
-                        <li>
-                            <a href="/">
-                                <CiCircleQuestion />
+                    {isWideScreen ? (
+                        <>
+                            <a
+                                className="navbar__menu"
+                                onClick={() => setOpenMenu(!openMenu)}
+                            >
+                                Menu
                             </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <TbWorld />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <TbUserCircle />
-                            </a>
-                        </li>
-                    </ul>
+                            <SideBar
+                                openMenu={openMenu}
+                                setOpenMenu={setOpenMenu}
+                            />
+                        </>
+                    ) : (
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    <CiCircleQuestion />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    <TbWorld />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    <TbUserCircle />
+                                </a>
+                            </li>
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
