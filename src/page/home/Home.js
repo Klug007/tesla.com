ff6afb3f054a307380319c50edea2dd03ad4a1f3
Klug007 <tilla.yosinbek@gmail.com>
2024-02-11 15:14:52 +0500

    fixed

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -6,20 +6,6 @@ function Home() {
     return (
         <div className="home">
             <Main />
-            <video
-                width="100%"
-                className="home__video"
-                autoPlay
-                muted
-                loop
-                playsInline
-            >
-                <source
-                    src="https://digitalassets.tesla.com/tesla-contents/video/upload/Homepage-Model-Y-Desktop-NA-v2.mp4"
-                    type="video/mp4"
-                />
-                Your browser does not support the video tag.
-            </video>
         </div>
     );
 }
